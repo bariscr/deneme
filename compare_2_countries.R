c6afa157a38c4fc2664cf54453a759dacccefcf4
Barış Uçar <barisucar@Barss-MBP.mynet>
2020-07-27 23:44:07 +0300

    Variable list amended.

--- compare_2_countries.R
+++ compare_2_countries.R
@@ -7,15 +7,16 @@ ui <- fluidPage(
   h1("COVID COMPARISON"),
   selectInput("country1", "Select country 1", choices = unique(covid_data_26072020$location)),
   selectInput("country2", "Select country 2", choices = unique(covid_data_26072020$location)),
-  selectInput("variable", "Select a variable to compare", choices = names(covid_data_26072020)),
+  selectInput("variable", "Select a variable to compare", 
+              choices = c("total_cases", "new_cases","total_deaths", "new_deaths",
+                          "total_cases_per_million", "new_cases_per_million",          
+                          "total_deaths_per_million", "new_deaths_per_million")),
   dateRangeInput("daterange", "Select a date range", start = "2019-12-31", end = "2020-07-25"),
   tableOutput("tablo"),
   mainPanel({
     plotOutput("graph")
   })
 )
-
-
 
 server <- function(input, output) {
   
