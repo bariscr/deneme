153e21abb46583e853a8a5f85803c89a0fb729c5
Barış Uçar <barisucar@Barss-MBP.mynet>
2020-07-29 00:45:25 +0300

    Shinydashboard.

--- compare_2_countries.R
+++ compare_2_countries.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(shinydashboard)
 library(ggplot2)
 library(dplyr)
 library(rio)
@@ -17,20 +18,42 @@ covid_data <- rio::import('https://github.com/owid/covid-19-data/raw/master/publ
                                "numeric", "numeric", "numeric", 
                                "numeric", "numeric"))
 
-ui <- fluidPage(
-  h1("COVID COMPARISON"),
-  selectInput("country1", "Select country 1", choices = unique(covid_data$location)),
-  selectInput("country2", "Select country 2", choices = unique(covid_data$location)),
-  selectInput("variable", "Select a variable to compare", 
-              choices = c("total_cases", "new_cases","total_deaths", "new_deaths",
-                          "total_cases_per_million", "new_cases_per_million",          
-                          "total_deaths_per_million", "new_deaths_per_million")),
-  dateRangeInput("daterange", "Select a date range", start = "2019-12-31", end = Sys.Date()),
-  tableOutput("tablo"),
-  mainPanel({
-    plotOutput("graph", brush = "click")
-  })
-)
+ui <-   dashboardPage(
+  skin = "purple",
+  dashboardHeader(title = "COVID-19 Dashboard"),
+  dashboardSidebar(sidebarMenu(
+    menuItem("Name this", tabName = "tab_1", icon = icon("file-alt")),
+    menuItem("Name this also", tabName = "tab_2", icon = icon("file-alt"))
+  )),
+  
+  # dashboard start ----
+  dashboardBody(tabItems(
+    
+    # password tab ----
+    tabItem("tab_1",
+            fluidPage(
+              h1("COVID COMPARISON"),
+              selectInput("country1", "Select country 1", choices = unique(covid_data$location)),
+              selectInput("country2", "Select country 2", choices = unique(covid_data$location)),
+              selectInput("variable", "Select a variable to compare", 
+                          choices = c("Total Cases" = "total_cases", "New Cases" = "new_cases",
+                                      "Total Deaths" = "total_deaths", "New Deaths" = "new_deaths",
+                                      "Total Cases per Million" = "total_cases_per_million", 
+                                      "New cases Per Million" = "new_cases_per_million", 
+                                      "Total Deaths per Million" = "total_deaths_per_million", 
+                                      "New Deaths per Million" = "new_deaths_per_million")),
+              dateRangeInput("daterange", "Select a date range", start = "2019-12-31", end = Sys.Date()),
+              tableOutput("tablo"),
+              mainPanel({
+                plotOutput("graph", brush = "click")
+              })
+            )),
+    
+    tabItem("tab_2",
+            fluidPage(
+              h1("Move it babe")
+ )))))
+
 
 server <- function(input, output) {
   
