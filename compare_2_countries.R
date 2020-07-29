a14e5dab4af4e87e84c69ad13eaf8f78cf26aa38
Barış Uçar <barisucar@Barss-MBP.mynet>
2020-07-29 20:23:03 +0300

    Infobox.

--- compare_2_countries.R
+++ compare_2_countries.R
@@ -6,7 +6,6 @@ library(rio)
 library(scales)
 library(purrr)
 
-covid_data$date <- as.Date(covid_data$date)
 # load data ----
 covid_data <- rio::import('https://github.com/owid/covid-19-data/raw/master/public/data/owid-covid-data.xlsx',
                  col_types = c("text", "text", "text", 
@@ -21,19 +20,30 @@ covid_data <- rio::import('https://github.com/owid/covid-19-data/raw/master/publ
                                "numeric", "numeric", "numeric", 
                                "numeric", "numeric"))
 
+covid_data$date <- as.Date(covid_data$date)
+
 ui <-   dashboardPage(
   skin = "purple",
   dashboardHeader(title = "COVID-19 Dashboard"),
   dashboardSidebar(sidebarMenu(
-    menuItem("Name this", tabName = "tab_1", icon = icon("file-alt")),
-    menuItem("Name this also", tabName = "tab_2", icon = icon("file-alt"))
+    menuItem("Country COVID-19 Profile", tabName = "tab_1", icon = icon("heart")),
+    menuItem("Name this also", tabName = "tab_2", icon = icon("file-alt")),
+    menuItem("Name this also2", tabName = "tab_3", icon = icon("file-alt"))
   )),
   
   # dashboard start ----
   dashboardBody(tabItems(
     
-    # password tab ----
+    # tab 1----
     tabItem("tab_1",
+            fluidRow(
+              selectInput("country_select_tab1", "Select Country", choices = unique(covid_data$location)),
+              dateInput("date_tab1", "Select a Date", value = Sys.Date()),
+              infoBoxOutput("approvalBox"),
+              infoBoxOutput("approvalBox2")
+            )),    
+
+    tabItem("tab_2",
             fluidPage(
               h1("COVID COMPARISON"),
               selectInput("country1", "Select country 1", choices = unique(covid_data$location)),
@@ -52,7 +62,7 @@ ui <-   dashboardPage(
               })
             )),
     
-    tabItem("tab_2",
+    tabItem("tab_3",
             fluidPage(
               selectInput("country_alone", "Select country", choices = unique(covid_data$location)),
               selectInput("variable_compare", "Select variables to compare", choices = c("Total Cases" = "total_cases", "New Cases" = "new_cases",
@@ -63,10 +73,29 @@ ui <-   dashboardPage(
                                                                                          "New Deaths per Million" = "new_deaths_per_million"), multiple = TRUE),
               dateRangeInput("daterange_alone", "Select a date range", start = "2019-12-31", end = Sys.Date()),
               plotOutput("plot_alone")
- )))))
+ ))
+)))
 
 
 server <- function(input, output) {
+  
+  output$approvalBox <- renderInfoBox({
+    infoBox(
+      "Number of Cases", 
+      covid_data$total_cases[covid_data$location == input$country_select_tab1 & covid_data$date == input$date_tab1], 
+      icon = icon("asterisk", lib = "glyphicon"),
+      color = "yellow"
+    )
+  })
+  
+  output$approvalBox2 <- renderInfoBox({
+    infoBox(
+      "Number of Deaths", 
+      covid_data$total_deaths[covid_data$location == input$country_select_tab1 & covid_data$date == input$date_tab1], 
+      icon = icon("warning-sign", lib = "glyphicon"),
+      color = "black"
+    )
+  })
   
   data <- reactive({
     covid_data %>% filter(location == input$country1 | location == input$country2, date >= input$daterange[1], date <= input$daterange[2])
@@ -95,8 +124,6 @@ server <- function(input, output) {
     }
     p
   })
-  
-  
 }
 
 shinyApp(ui, server)
